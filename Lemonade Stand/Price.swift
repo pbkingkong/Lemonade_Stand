f3d2d8515a8b964fe999a121abfffe27815f56aa
Peter <peterbuzek@alexanders-imac.fritz.box>
2014-10-30 18:59:22 +0100

    first commit

---
+++ Lemonade Stand/Price.swift
@@ -0,0 +1,9 @@
+//
+//  Price.swift
+//  Lemonade Stand
+//
+//  Created by Peter on 30.10.14.
+//  Copyright (c) 2014 PB King. All rights reserved.
+//
+
+import Foundation
---
+++ Lemonade Stand/Supplies.swift
@@ -0,0 +1,9 @@
+//
+//  Supplies.swift
+//  Lemonade Stand
+//
+//  Created by Peter on 30.10.14.
+//  Copyright (c) 2014 PB King. All rights reserved.
+//
+
+import Foundation
